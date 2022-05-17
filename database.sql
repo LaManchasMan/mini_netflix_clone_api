98c96e36f1536f98f80c073e59785e3f6ec9f295
Enoch <enoch4kigwila@gmail.com>
2022-05-17 15:35:22 -0400

    adding database to the database.sql

--- database.sql
+++ database.sql
@@ -3,6 +3,34 @@ CREATE DATABASE Netflix;
 CREATE TABLE  users(
     id SERIAL PRIMARY KEY,
     name varchar(50) not null,
-    email varchar(50) not null,
+    email varchar(50) not null
 );
  
+ INSERT INTO users 
+        (name, email)
+    VALUES ('enoch', 'hoseaemail@gmail.com');
+
+ALTER TABLE users
+ADD password varchar(50);
+
+INSERT INTO users 
+        (name, email, password)
+    VALUES ('hosea', 'hoseaemail@gmail.com', 'kigwila');
+
+
+    CREATE TABLE  category(
+    id SERIAL PRIMARY KEY,
+    anime varchar(50) not null,
+    action varchar(50) not null,
+    drama varchar (50) not null
+    );
+
+INSERT INTO category 
+        (anime, action, drama)
+    VALUES ('Godzilla', 'Furioza', 'Breaking Bad'), 
+            ('Berserk: The Golden Age Arc III', 'Hard Kill', 'Bridgerton'),
+            ('Naruto shippuden: blood prison', 'Sentinelle', 'The Queen Gambit'),
+            ('Attack on Titans', 'Inside Man', 'Curon'),
+            ('Demon slayer', 'The Bourne Ultimation', 'The Hundred'),
+            ('Trigun', 'Below Zero', 'Lost Girls');
+            
