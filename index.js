3fea855c2828cc94a30ec8926f90a81cbc23d4da
Zay <hosea8zay@gmail.com>
2022-05-20 12:45:34 -0400

    added more routes

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const http = require("http");
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -7,14 +5,33 @@ const express = require('express');
 const app = express();
 const server = http.createServer(app);
 
-
-
 app.get('/', (req,res)=>
+res.send('home page')
+)
+
+
+app.get('/signup', (req,res)=>
 res.redirect('https://nomaddev.animaapp.io/')
 )
+
+app.get('/Home', (req,res)=>
+res.redirect('https://nomaddev.animaapp.io/home')
+)
+
+app.get('/Content-page', (req,res)=>
+res.redirect('https://nomaddev.animaapp.io/content-page')
+)
+
+
+
  
 app.listen(3000)
 
 server.listen(port, hostname, () => {
         console.log(`Server running at http://${hostname}:${port}/`);
      });
+
+     function logs(req, res, next){
+             console.log('log')
+             next()
+     }
